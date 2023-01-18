7aa578834b796e6b4d5ea9aab1bfdc7f5eae9c77
HPeti <hpeti0912@gmail.com>
2023-01-18 15:23:22 +0100

    Modified movie attributes

--- basic_rest_api_app/models.py
+++ basic_rest_api_app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Float, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -9,19 +9,5 @@ class Movie(Base):
     id = Column(Integer, primary_key=True, index = True)
     title = Column(String, unique = True, index = True)
     year = Column(Integer)
-    is_adult = Column(Boolean, default=False)
-    runtime_minutes = Column(Integer)
-    genres = Column(String)
-
-#class Movie(Base):
-#    __tablename__ = "movies"
-#
-#    tconst = Column(String, primary_key=True, index = True)
-#    titleType = Column(String)
-#    primaryTitle = Column(String, unique = True, index = True)
-#    originalTitle = Column(String, unique = True, index = True)
-#    isAdult = Column(String)
-#    startYear = Column(String)
-#    endYear = Column(String)
-#    runtimeMinutes = Column(String)
-#    genres = Column(String)
+    rating = Column(Float)
+    casts = Column(String)
--- basic_rest_api_app/schemas.py
+++ basic_rest_api_app/schemas.py
@@ -3,19 +3,18 @@ from pydantic import BaseModel
 # Schema to have common attributes while creating or reading data.
 class MovieBase(BaseModel):
     title: str
-    year: int | None = None
-    is_adult: bool
-    runtime_minutes: int
-    genres: str
+    year: int
+    rating: float
+    casts: str
 
 # And create class that inherits from it, plus any additional data (attributes) needed for creation.
 class MovieCreate(MovieBase):
     pass
 
-# You can also add other attributes, like a list of genres. (maybe TODO)
+# You can also add other attributes, like a list of casts. (maybe TODO)
 # Move ID and other things inside DB here instead of MovieBase?
 class Movie(MovieBase):
-    id: str
+    id: int
 
     class Config:
         orm_mode = True
